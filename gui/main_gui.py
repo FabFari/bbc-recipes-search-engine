604cbc6e99fc8d73e5d24f32b07e4912ef26131f
Sk3pper <abissoli.ab@gmail.com>
2016-11-09 16:58:22 +0100

    refactor gui code

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,23 +1,14 @@
-import io
 import sys
 import webbrowser
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtCore import QUrl
 from gui.main_view import Ui_MainWindow
-from gui.web_view import Ui_WebContent
 from search_engine.query_engine import perform_query
 from search_engine.query_engine import setup_query_engine
-from bs4 import BeautifulSoup
-from search_engine.corpus_builder import check_if_empty
-from PyQt4.QtGui import *
 import urllib2
-from PyQt4.QtCore import QUrl
-from PyQt4.QtNetwork import QNetworkAccessManager, QNetworkRequest
-import time
 from multiprocessing.pool import ThreadPool
-from utils.data_structures import DocEntry
 
 RECIPES_DIR = "recipes"
+
 
 class QCustomQWidget(QtGui.QWidget):
     def __init__(self, parent=None):
@@ -36,27 +27,18 @@ class QCustomQWidget(QtGui.QWidget):
         self.textUpQLabel.setStyleSheet(''' color: rgb(0, 0, 255); ''')
         self.textDownQLabel.setStyleSheet('''color: rgb(255, 0, 0); ''')
 
-    def text(self):
-        print 'fewe'
-
-    def setNameFile(self, text):
+    def set_name_file(self, text):
         self.name_file = text
 
-    def setTextUp(self, text):
+    def set_text_up(self, text):
         self.textUpQLabel.setText(text)
 
-    def setTextDown(self, text):
+    def set_text_down(self, text):
         self.textDownQLabel.setText(text)
 
-    def setIcon(self, imagePath):
-        if(len(imagePath)>0):
-            #img =urllib.urlretrieve(imagePath, str(self.name_file)+".jpg")
-
-            # print 'imagePath: '+str(imagePath)
-            # start = time.time()
-            data = urllib2.urlopen(imagePath).read()
-            # end = time.time()
-            # print(end - start)
+    def set_icon(self, image_path):
+        if len(image_path) > 0:
+            data = urllib2.urlopen(image_path).read()
             image = QtGui.QImage()
             image.loadFromData(data)
             width = 40
@@ -64,23 +46,11 @@ class QCustomQWidget(QtGui.QWidget):
             self.iconQLabel.setMinimumSize(width, height)
             self.iconQLabel.setMaximumSize(width, height)
             self.iconQLabel.resize(width, height)
-            pixmap = QtGui.QPixmap(image)
-            self.pixmap2 = pixmap.scaledToWidth(74)
-            #mg.setMinimumSize(width, height)
-            #img.setMaximumSize(width, height)
-            #img.resize(width,height)
-            self.iconQLabel.setPixmap(self.pixmap2)
+            self.pixmap = QtGui.QPixmap(image)
+            self.pixmap = pixmap.scaledToWidth(74)
+            self.iconQLabel.setPixmap(self.pixmap)
 
-    def getTextUp(self):
-        return 'getTextUp'
-
-    def getTextDown(self):
-        return 'getTextDown'
-
-    def getIcon(self):
-        return 'getIcon'
-
-    def getNameFile(self):
+    def get_name_file(self):
         return self.name_file
 
 
@@ -103,57 +73,56 @@ class Window(QtGui.QMainWindow, Ui_MainWindow):
         async_result = pool.apply_async(self.worker, (query, self.checkBox.isChecked()))  # tuple of args for foo
         result = async_result.get()
 
-        listWidget = self.listWidget
-        listWidget.clear()
+        list_widget = self.listWidget
+        list_widget.clear()
         for r in result:
             # Create QCustomQWidget
-            myQCustomQWidget = QCustomQWidget()
+            my_q_custom_q_widget = QCustomQWidget()
 
-            myQCustomQWidget.setNameFile(r.get_name())
-            myQCustomQWidget.setTextUp(r.get_title())
-            myQCustomQWidget.setTextDown(r.get_desc())
-            myQCustomQWidget.setIcon(r.get_img_url())
+            my_q_custom_q_widget.set_name_file(r.get_name())
+            my_q_custom_q_widget.set_text_up(r.get_title())
+            my_q_custom_q_widget.set_text_down(r.get_desc())
+            my_q_custom_q_widget.set_icon(r.get_img_url())
 
             # Create QListWidgetItem
-            myQListWidgetItem = QtGui.QListWidgetItem(listWidget)
+            myQListWidgetItem = QtGui.QListWidgetItem(list_widget)
 
             # add data to retrive when we click the single item
-            myQListWidgetItem.setData(QtCore.Qt.UserRole, myQCustomQWidget)
+            myQListWidgetItem.setData(QtCore.Qt.UserRole, my_q_custom_q_widget)
 
             # Set size hint
-            myQListWidgetItem.setSizeHint(myQCustomQWidget.sizeHint())
+            myQListWidgetItem.setSizeHint(my_q_custom_q_widget.sizeHint())
 
             # Add QListWidgetItem into QListWidget
-            listWidget.addItem(myQListWidgetItem)
-            listWidget.setItemWidget(myQListWidgetItem, myQCustomQWidget)
+            list_widget.addItem(myQListWidgetItem)
+            list_widget.setItemWidget(myQListWidgetItem, my_q_custom_q_widget)
 
-        # self.setCentralWidget(listWidget)
-        listWidget.itemClicked.connect(self.item_click)
-        listWidget.show()
+        # self.setCentralWidget(list_widget)
+        list_widget.itemClicked.connect(self.item_click)
+        list_widget.show()
 
         self.go_button.setEnabled(True)
         return
 
+    @staticmethod
     def worker(self, query, check):
-        """thread worker function"""
         print '[main_gui] perform_query "' + str(query) + '"'
         if str(query) in 'type the query':
             print '[main_gui] type the query in the query'
         else:
             result = perform_query(str(query),check)
-            # id, name, size, title, desc, img_url, veggie=False
-            #result = [DocEntry(0,"prova",87,"potato","baked_ginger_parkin_with_52330","http://weknowyourdreams.com/image.php?pic=/images/apple/apple-06.jpg")]
+
             if len(result) == 0:
                 result.append('No result found')
-            # print '[main_gui]' + str(result)
             return result
 
-    def item_click(self, item):
-        print "[main_gui] You clicked: " + str(item.data(QtCore.Qt.UserRole).toPyObject().getNameFile())
+    @staticmethod
+    def item_click(item):
+        print "[main_gui] You clicked: " + str(item.data(QtCore.Qt.UserRole).toPyObject().get_name_file())
         # now we have to lauch the new window
-        url = 'http://www.bbc.co.uk/food/recipes/'+str(item.data(QtCore.Qt.UserRole).toPyObject().getNameFile())
-        # url = 'http://www.bbc.co.uk/food/recipes/cappucino_crme_brles_08725'
+        url = 'http://www.bbc.co.uk/food/recipes/'+str(item.data(QtCore.Qt.UserRole).toPyObject().get_name_file())
         webbrowser.open(url)
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     win = Window()
