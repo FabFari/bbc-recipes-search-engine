07449d7c846c53a36bf88b8ce05be3436faa5218
Tommaso d'Orsi <tommaso@localhost.lan>
2016-11-13 14:03:31 +0100

    Query engine commented

--- search_engine/query_engine.py
+++ search_engine/query_engine.py
@@ -12,21 +12,26 @@ from inv_index_builder import ING_WEIGHT
 
 
 INPUT_DIR = "data"
-
+#FIle name of the inverted index
 INDEX_NAME = "inverted_index.json"
+#File name of the collection of documents
 JSON_NAME = "documents.json"
+
 TSV_NAME = "recipes_tags.tsv"
 
+#Term-Document 's with an idf score < THRESHOLD will be ignored
 THRESHOLD = 0.0
+#Documents with many query terms will have their score boosted
 WORDS_STEAK_BONUS = 0.1
 PROX_SCORE_FACTOR = 0.5
+#Tag for vegetarian recipes
 VEGGIE = "Vegetarian"
 
 # Global Data Structures
 dictionary = {}
 documents = []
 
-
+#NON SO COSA FA
 def deunify_dict(d):
     no_utf_d = {}
 
@@ -35,14 +40,14 @@ def deunify_dict(d):
 
     return no_utf_d
 
-
+#NON SO COSA FA
 def load_json_to_str(filename, decoder):
     with open(os.path.join(os.pardir, INPUT_DIR, filename)) as json_data:
         data = json.load(json_data, cls=decoder)
 
     return deunify_dict(data)
 
-
+#SI PUÒ RIMUOVERE SECONDO ME
 def compute_len_ingr(ing_list, process):
     len_ingr = 0
     print "ing_list: "+str(len(ing_list))
@@ -51,6 +56,8 @@ def compute_len_ingr(ing_list, process):
     return len_ingr
 
 
+#This method  is used to load in memory the dictionary and the documents.
+#This data structures are then used to perform the user queries
 def setup_query_engine():
     print 'setup_query_engine..'
     global dictionary
@@ -60,6 +67,9 @@ def setup_query_engine():
     dictionary = load_json_to_str(INDEX_NAME, LabeledListDecoder)
 
 
+#This method is called upon the arrival of a query
+#It  glues together the retrieval of the relevant posting lists and 
+#the scoring of the documents.
 def perform_query(query, vegetarian=False):
     do_proximity = False
 
@@ -76,6 +86,10 @@ def perform_query(query, vegetarian=False):
     return res
 
 
+#To retrieve all the documents containing query terms.
+#This method is to be used to retrieve the posting lists of the query terms
+#The query is first preprocessed, then, the so-obtained words are used to get the
+#lists from the  dictionary.
 def retrieve_docs(query):
     # Preprocessing query
     print 'Preprocessing query..'
@@ -86,7 +100,9 @@ def retrieve_docs(query):
     retrieved_postings = []
     # i = 0
     print 'retrieving posting lists..'
-
+	 
+	 #For each word in the processed query
+	 #Retrieve the corresponding posting list.
     for word in q_tokenized:
         # i += 1
         try:
@@ -94,27 +110,38 @@ def retrieve_docs(query):
         except KeyError:
             continue
         idf = retrieved_posting.get_value()
+        #idf scores < THRESHOLD will be ignored
         if idf > THRESHOLD:
             retrieved_postings.append(retrieved_posting)
 
-    # print i
     return retrieved_postings
 
 
-# Receive as input the posting lists retrieved
-# Compute all the cosine similarities in parallel, returns the top 10
+#Receiving as input the posting lists retrieved,
+#this method is to be used to compute the relevance, with respect to the query, of the documents retrieved.
+#The method is called for each query and returns the top 10 scoring documents.
+#Documents' scores are computed in parallel;
+#The specific term-document tf idf are already precomputed.
 def compute_scores(posting_lists, do_proximity=False, vegetarian=False):
     global documents
     doc_scores = defaultdict(lambda: 0)
 
+    #Structures needed to perform phrase queries
     if do_proximity:
         doc_pos = {}
         pair_docs = {}
         prox_score = defaultdict(lambda: 0)
-
+	 
+	 #Compute the score for each document appearing in any of the posting lists retrieved
     for cur_list in posting_lists:
         docs = cur_list.get_my_list()
         idf = cur_list.get_value()
+        
+        #For each document in the current posting list,
+        #Retrieve the tf and idf scores
+        #Compute the norm taking into account the length and the weight of the different parts of the document
+        #The vegetarian flag, if set, filter out all the non vegetarian results.
+        #The do_proximity flag, if set, perform phrase queries
         for d in docs:
             doc_id = d.get_label()
 
@@ -123,10 +150,11 @@ def compute_scores(posting_lists, do_proximity=False, vegetarian=False):
             tf = d.get_value()
             cur_value = doc_scores[doc_id]
             bonus = 0
-
+				
+				#Fabri come dicevi ieri tu dovremmo toglierlo questo bonus ora.
             if cur_value > 0:
                 bonus = WORDS_STEAK_BONUS
-
+				
             cur_doc = documents[str(doc_id)]
             norm = float(cur_doc.get_size()) + cur_doc.get_title_size()*TITLE_WEIGHT + cur_doc.get_size_ingr()*ING_WEIGHT
             doc_scores[doc_id] = bonus + cur_value + ((tf * idf)/norm)
@@ -142,7 +170,8 @@ def compute_scores(posting_lists, do_proximity=False, vegetarian=False):
                     pair_docs[doc_id].add((cur_list.get_label(), "NONE"))
                 else:
                     pair_docs[doc_id] = extend_pairs(pair_docs[doc_id], cur_list.get_label())
-
+	
+	 #NON SO COME FUNZIONA
     if do_proximity and len(pair_docs) > 0:
         for doc_id, pairs in pair_docs.iteritems():
             for terms in pairs:
@@ -155,7 +184,8 @@ def compute_scores(posting_lists, do_proximity=False, vegetarian=False):
                 doc_scores[doc_id] += prox_score[doc_id]
             except KeyError:
                 continue
-
+                
+	 #Order the results in descending order of their score.
     if vegetarian:
         veggie_docs = {str(doc_id): score for doc_id, score in doc_scores.iteritems() if documents[str(doc_id)].is_veggie()}
         ordered_docs = sorted(veggie_docs.items(), key=operator.itemgetter(1), reverse=True)
@@ -166,7 +196,7 @@ def compute_scores(posting_lists, do_proximity=False, vegetarian=False):
 
     return res
 
-
+#DA COMMENTARE PER PHRASE QUERIES
 def extend_pairs(pair_docs, doc_id):
     new_pairs = set()
 
@@ -177,7 +207,7 @@ def extend_pairs(pair_docs, doc_id):
 
     return new_pairs
 
-
+#DA COMMENTARE PER PHRASE QUERIES
 def ev_dist(pos_list_i, pos_list_j):
     min_dist = abs(pos_list_i[0] - pos_list_j[0])
 
